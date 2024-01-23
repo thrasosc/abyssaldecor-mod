dfa61e641dd4c7b8ffb81439bba33d5fdfab3748
steficy <stefanos3110grthe@gmail.com>
2024-01-23 17:02:18 +0200

    Added more items

--- common/src/main/java/net/abyssaldecor/registry/ItemRegistry.java
+++ common/src/main/java/net/abyssaldecor/registry/ItemRegistry.java
@@ -12,6 +12,7 @@ public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
     public static final RegistrySupplier<Item> GARGOYLE_ITEM = ITEMS.register("gargoyleitem", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
     public static final RegistrySupplier<Item> AMARANTH_ITEM = ITEMS.register("amaranthitem", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> AMARANTH_SEEDS = ITEMS.register("amaranthseeds", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
 
     public static void init() {
         ITEMS.register();
