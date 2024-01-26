de0cee2500722aef40de4fcb992fe05689355f01
steficy <stefanos3110grthe@gmail.com>
2024-01-26 04:09:23 +0200

    Added more items /

--- common/src/main/java/net/abyssaldecor/registry/ItemRegistry.java
+++ common/src/main/java/net/abyssaldecor/registry/ItemRegistry.java
@@ -21,7 +21,17 @@ public class ItemRegistry {
     public static final RegistrySupplier<Item> BOG_APPLE_LEAVES = ITEMS.register("bog_apple_leaves", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
     public static final RegistrySupplier<Item> BOG_APPLE = ITEMS.register("bog_apple", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
     public static final RegistrySupplier<Item> BOG_APPLE_PIE = ITEMS.register("bog_apple_pie", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
-
+    public static final RegistrySupplier<Item> BONEROT = ITEMS.register("bonerot", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> BOTTOMLESS_BAG_OF_COBBLESTONE = ITEMS.register("bottomless_bag_of_cobblestone", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> BOTTOMLESS_BAG_OF_DIRT = ITEMS.register("bottomless_bag_of_dirt", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> BOTTOMLESS_BAG_OF_NETHERRACK = ITEMS.register("bottomless_bag_of_netherrack", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> BOTTOMLESS_BAG_OF_SNOW = ITEMS.register("bottomless_bag_of_snow", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> SEABRASS_CHAIN = ITEMS.register("seabrass_chain", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> SEABRASS_DOOR = ITEMS.register("seabrass_door", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> SEABRASS_INGOT = ITEMS.register("seabrass_ingot", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> DEEPBRONZE_CHAIN = ITEMS.register("deepbronze_chain", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> DEEPBRONZE_DOOR = ITEMS.register("deepbronze_door", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> DEEPBRONZE_INGOT = ITEMS.register("deepbronze_ingot", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
 
 
     public static void init() {
