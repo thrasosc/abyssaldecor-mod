702969f3e2eb02a39ff6081e3150c10f75dcb4f0
steficy <stefanos3110grthe@gmail.com>
2024-01-26 21:08:22 +0200

    Added more items /

--- common/src/main/java/net/abyssaldecor/registry/ItemRegistry.java
+++ common/src/main/java/net/abyssaldecor/registry/ItemRegistry.java
@@ -32,6 +32,23 @@ public class ItemRegistry {
     public static final RegistrySupplier<Item> DEEPBRONZE_CHAIN = ITEMS.register("deepbronze_chain", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
     public static final RegistrySupplier<Item> DEEPBRONZE_DOOR = ITEMS.register("deepbronze_door", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
     public static final RegistrySupplier<Item> DEEPBRONZE_INGOT = ITEMS.register("deepbronze_ingot", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> DEEPBRONZE_NUGGET = ITEMS.register("deepbronze_nugget", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> CINNAMON_APPLE = ITEMS.register("cinnamon_apple", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> CINNAMON_DOOR = ITEMS.register("cinnamon_door", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> CINNAMON_ROLL = ITEMS.register("cinnamon_roll", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> CINNAMON_STICK = ITEMS.register("cinnamon_stick", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> CINNAMON_TEA = ITEMS.register("cinnamon_tea", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> DANGLING_WEB_ITEM = ITEMS.register("dangling_web_item", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> DULL_IRON_BAR_DOOR = ITEMS.register("dull_iron_bar_door", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM = ITEMS.register("fever_blossom", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_CROP_1 = ITEMS.register("fever_blossom_crop_1", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_CROP_2 = ITEMS.register("fever_blossom_crop_2", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_CROP_3 = ITEMS.register("fever_blossom_crop_3", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_CROP_4 = ITEMS.register("fever_blossom_crop_4", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_CROP_5 = ITEMS.register("fever_blossom_crop_5", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_CROP_6 = ITEMS.register("fever_blossom_crop_6", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_CROP_7 = ITEMS.register("fever_blossom_crop_7", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
+    public static final RegistrySupplier<Item> FEVER_BLOSSOM_TEA = ITEMS.register("fever_blossom_tea", () -> new Item(new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
 
 
     public static void init() {
