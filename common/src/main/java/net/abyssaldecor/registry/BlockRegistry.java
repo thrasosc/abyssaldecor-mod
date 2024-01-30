38ad8ff9a2d65e278096daa8b678681a6aae1e5f
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-01-31 00:50:26 +0100

    Added test block

--- common/src/main/java/net/abyssaldecor/registry/BlockRegistry.java
+++ common/src/main/java/net/abyssaldecor/registry/BlockRegistry.java
@@ -7,6 +7,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import static net.abyssaldecor.AbyssalDecor.MOD_ID;
@@ -16,8 +17,12 @@ public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, Registries.BLOCK);
     //TODO fix item texture of abyssal lantern, also something is causing crashes on Forge
     public static final RegistrySupplier<Block> ABYSSAL_LANTERN = registerBlockWithItem(
-            "abyssal_lantern", new AbyssalLanternBlock(BlockBehaviour.Properties.of().lightLevel(l -> 15))
+            "abyssal_lantern", new AbyssalLanternBlock(BlockBehaviour.Properties.copy(Blocks.REDSTONE_LAMP))
     );
+    public static final RegistrySupplier<Block> TEST_BLOCK = registerBlockWithItem(
+            "test_block", new Block(BlockBehaviour.Properties.copy(Blocks.ACACIA_WOOD))
+    );
+
     private static RegistrySupplier<Block> registerBlockWithItem(String name, Block block) {
         ItemRegistry.ITEMS.register(name, () -> new BlockItem(block, new Item.Properties().arch$tab(ABYSSALDECOR_TAB)));
         return BLOCKS.register(name, () -> block);
