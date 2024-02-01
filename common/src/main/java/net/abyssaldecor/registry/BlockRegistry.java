421d54c57e064d7328e8aec45b87658ad4e65244
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-02-01 20:41:18 +0100

    Removed unnecessary imports

--- common/src/main/java/net/abyssaldecor/registry/BlockRegistry.java
+++ common/src/main/java/net/abyssaldecor/registry/BlockRegistry.java
@@ -1,12 +1,10 @@
 package net.abyssaldecor.registry;
 
-import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.abyssaldecor.block.AbyssalLanternBlock;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
