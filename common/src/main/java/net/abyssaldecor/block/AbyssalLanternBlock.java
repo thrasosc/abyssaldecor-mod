94b5c7c21f2fcb57676e2141ee1ebb38cfc4c29e
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-02-01 23:13:49 +0100

    Fixed right-click

--- common/src/main/java/net/abyssaldecor/block/AbyssalLanternBlock.java
+++ common/src/main/java/net/abyssaldecor/block/AbyssalLanternBlock.java
@@ -1,6 +1,8 @@
 package net.abyssaldecor.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -16,8 +18,8 @@ public class AbyssalLanternBlock extends RedstoneLampBlock {
 
     public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
         if (interactionHand == InteractionHand.MAIN_HAND && player.getItemInHand(interactionHand).isEmpty()) {
-            boolean b = (boolean) blockState.getValue(LIT);
-            this.defaultBlockState().setValue(LIT, !b);
+            level.playSound(player, blockPos, SoundEvents.FLINTANDSTEEL_USE, SoundSource.BLOCKS, 1f, 1f);
+            level.setBlock(blockPos, (BlockState)blockState.cycle(LIT), 2);
             return InteractionResult.SUCCESS;
         }
         return InteractionResult.PASS;
