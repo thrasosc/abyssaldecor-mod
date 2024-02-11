31fb1b7724b417443757b15567d44f5a4a528643
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-02-11 22:37:20 +0100

    Add forge client method

--- forge/src/main/java/net/abyssaldecor/forge/AbyssalDecorForge.java
+++ forge/src/main/java/net/abyssaldecor/forge/AbyssalDecorForge.java
@@ -2,14 +2,22 @@ package net.abyssaldecor.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import net.abyssaldecor.AbyssalDecor;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AbyssalDecor.MOD_ID)
 public class AbyssalDecorForge {
     public AbyssalDecorForge() {
+        IEventBus eventBus= FMLJavaModLoadingContext.get().getModEventBus();
         // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(AbyssalDecor.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        EventBuses.registerModEventBus(AbyssalDecor.MOD_ID, eventBus);
+        eventBus.addListener(this::clientSetup);
         AbyssalDecor.init();
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        AbyssalDecor.initClient();
     }
 }
