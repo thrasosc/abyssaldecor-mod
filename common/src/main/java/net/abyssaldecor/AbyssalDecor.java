3271ddb6e73fe43ceb93e62de2ff007d3947ae53
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-02-25 21:22:01 +0100

    Add Azurelib

--- common/src/main/java/net/abyssaldecor/AbyssalDecor.java
+++ common/src/main/java/net/abyssaldecor/AbyssalDecor.java
@@ -1,6 +1,7 @@
 package net.abyssaldecor;
 
 import dev.architectury.registry.client.rendering.RenderTypeRegistry;
+import mod.azure.azurelib.AzureLib;
 import net.abyssaldecor.registry.BlockRegistry;
 import net.abyssaldecor.registry.ItemGroupRegistry;
 import net.abyssaldecor.registry.ItemRegistry;
@@ -10,6 +11,7 @@ public class AbyssalDecor {
     public static final String MOD_ID = "abyssaldecor";
 
     public static void init() {
+        AzureLib.initialize();
         System.out.println(ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
         ItemGroupRegistry.init();
         ItemRegistry.init();
